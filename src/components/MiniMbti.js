7efecfc6eecc2025ec7c72206ff2edacf0d927bd
leedg919 <117734848+leedg919@users.noreply.github.com>
2023-02-16 17:55:39 +0900

    3nd modify

--- src/components/MiniMbti.js
+++ src/components/MiniMbti.js
@@ -3,12 +3,14 @@ import '../css/MiniMbti.css';
 const MiniMbti = ({data}) => {
     return (
         <div className='miniContainer'>
-            <div className="miniTitles">
-                <div className='miniTitle'>{data.title}</div>
-                <div className='miniTitle'>{data.title2}</div>
-            </div>
-            <div className="miniImgBox">
-                <img className="miniImg" src={data.img} />
+            <div className='miniItems'>
+                    <div className="miniTitles">
+                        <div className='miniTitle'>{data.title}</div>
+                        <div className='miniTitle'>{data.title2}</div>
+                    </div>
+                    <div className="miniImgBox">
+                        <img className="miniImg" src={data.img} />
+                    </div>
             </div>
         </div>
     );
